5be6e5684674559991ca62bd2d225028ccd1f289
Koubek <vanak.jakub@gmail.com>
2017-06-15 18:29:25 +0200

    Fix build script (download into non-existing folder). Use powershell instead of BAT.

--- build.bat
+++
@@ -1,3 +0,0 @@
-powershell -command Invoke-WebRequest -Uri "http://go.microsoft.com/fwlink/?LinkID=615137" -OutFile ".\Install\rewrite_amd64.msi"
-docker rmi navdocker.azurecr.io/nav/generic
-docker build -t navdocker.azurecr.io/nav/generic .
---
+++ build.ps1
@@ -0,0 +1,10 @@
+$installPath = Join-Path $PSScriptRoot 'Install'
+if (!(Test-Path $installPath))
+{
+    New-Item -ItemType Directory -Force $installPath
+}
+
+Invoke-WebRequest -Uri "http://go.microsoft.com/fwlink/?LinkID=615137" -OutFile (Join-Path $installPath 'rewrite_amd64.msi')
+
+docker rmi navdocker.azurecr.io/nav/generic
+docker build -t navdocker.azurecr.io/nav/generic .
